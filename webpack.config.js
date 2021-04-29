8e5aa6e4af7206e2e4aac0f7c25bb4153a004632
Maria Bermudez <bermudezmt@users.noreply.github.com>
2021-04-28 20:36:13 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,27 @@
+const DotEnvWebpack = require('dotenv-webpack');
 module.exports = (config, { isProd, isDev, isTest }) => {
-  /**
-   * Customize the webpack by modifying the config object.
-   * Consult https://webpack.js.org/configuration for more information
-   */
-
-  return config;
+    /**
+     * Customize the webpack by modifying the config object.
+     * Consult https://webpack.js.org/configuration for more information
+     */
+    // We dynamically change the path to the .env that contains the file corresponding to our profile
+    let envPath;
+    switch (process.env.TWILIO_PROFILE) {
+        case 'Dev':
+            envPath = '.env.dev';
+            break;
+        case 'Stage':
+            envPath = '.env.stage';
+            break;
+        case 'Production':
+            envPath = '.env.prod';
+            break;
+    }
+    // If path was set, use the dotenv-webpack to inject the variables
+    if (envPath) {
+        config.plugins.push(new DotEnvWebpack({
+            path: envPath
+        }));
+    }
+    return config;
 }
