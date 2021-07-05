d94073b36639c5c0b1381b6523d2cabc95b66b95
Giuseppe Verni <gverni@twilio.com>
2021-07-05 09:06:38 +0100

    Add options for VoiceMail handling

--- public/resources/functions/options.private.js
+++ public/resources/functions/options.private.js
@@ -7,4 +7,10 @@ module.exports = {
   statPeriod: 5,
   // Enable Queue Position in voice prompt
   getQueuePosition: true,
+  // Priority for the Task generatared by the VoiceMail
+  VoiceMailTaskPriority: 50,
+  // Agent audible alert sound file - task attribute value
+  VoiceMailAlertTone: '/assets/alertTone.mp3',
+  // Timezone configuration
+  TimeZone: 'America/Los_Angeles',
 };
